aa312594cf22718e14b61e32b73c845a13b1febb
loganploegman5-commits <loganploegman5@gmail.com>
2026-01-17 16:54:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ setInterval(() => {
 // U CAN ONLY EDIT THIS SECTION!!
 function createBot () {
 const bot = mineflayer.createBot({
-  host: 'sgshowriyaff.aternos.me', 
+  host: 'Lifesteal-yCZK.aternos.me', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'SG_SHOWRIYA', 
-  port: 28648, 
+  username: 'SG_BOT', 
+  port: 30278, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
 })
